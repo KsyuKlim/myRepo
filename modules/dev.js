5d437c74935a92b6a30d9c48b021935b44e7d19d
KsyuKlim <aksyuta13@gmail.com>
2023-09-12 15:25:37 +0300

    created new homework

---
+++ modules/dev.js
@@ -0,0 +1,22 @@
+import { Employee } from "./employee.js";
+
+class Dev extends Employee {
+  getJobType() {
+    return "I am a Dev";
+  }
+}
+
+export { Dev };
+
+class Dev extends Employee {
+  constructor(id, salary) {
+    super(id, salary);
+    Object.defineProperty(this, "id", { writable: false });
+  }
+
+  getJobType() {
+    return "I am a Dev";
+  }
+}
+
+export { Dev };
---
+++ modules/pm.js
@@ -0,0 +1,9 @@
+import { Employee } from "./employee.js";
+
+class PM extends Employee {
+  getJobType() {
+    return "I am a PM";
+  }
+}
+
+export { PM };
